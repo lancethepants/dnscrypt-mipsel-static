6a2e9c0bdecb4caa24a0ea9e6224cc4b1c2f2a1a
lancethepants <lancethepants@gmail.com>
2014-04-29 10:24:53 -0600

    Add dnscrypt-proxy build script. Update README.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 dnscrypt-mipsel-static
 ======================
+
+Script to create static dnscrypt binaries for mipsel devices using the entware toolchain.
+
+Make sure the entware toolchain is already in your PATH, and run.
+
+The resulting binaries are available at http://lancethepants.com/files
---
+++ dnscrypt.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir ~/dnscrypt && cd ~/dnscrypt
+
+BASE=`pwd`
+SRC=$BASE/src
+WGET="wget --prefer-family=IPv4"
+DEST=$BASE/opt
+LDFLAGS="-L$DEST/lib -Wl,--gc-sections"
+CPPFLAGS="-I$DEST/include"
+CFLAGS="-mtune=mips32 -mips32 -O3 -ffunction-sections -fdata-sections"	
+CXXFLAGS=$CFLAGS
+CONFIGURE="./configure --prefix=/opt --host=mipsel-linux"
+MAKE="make -j`nproc`"
+mkdir -p $SRC
+
+############# ###############################################################
+# LIBSODIUM # ###############################################################
+############# ###############################################################
+
+mkdir -p $SRC/sodium && cd $SRC/sodium
+$WGET https://github.com/jedisct1/libsodium/releases/download/0.4.5/libsodium-0.4.5.tar.gz
+tar zxvf libsodium-0.4.5.tar.gz
+cd libsodium-0.4.5
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE \
+--enable-static \
+--disable-shared
+
+$MAKE
+make install DESTDIR=$BASE
+
+############ ################################################################
+# DNSCRYPT # ################################################################
+############ ################################################################
+
+mkdir $SRC/dnscrypt && cd $SRC/dnscrypt
+$WGET http://download.dnscrypt.org/dnscrypt-proxy/dnscrypt-proxy-1.4.0.tar.gz
+tar zxvf dnscrypt-proxy-1.4.0.tar.gz
+cd dnscrypt-proxy-1.4.0
+
+LDFLAGS="-Wl,-static -static -static-libgcc -s $LDFLAGS" \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE
+
+$MAKE
+make install DESTDIR=$BASE/dnscrypt
